a994a676c0a7a01ec13f9acfcea6cc58171ef82e
Mickeyxiami <xiami@mickey.moe>
2017-01-04 18:20:43 +0800

    update

--- src/main/java/moe/mickey/forge/nonupdate/Config.java
+++ src/main/java/moe/mickey/forge/nonupdate/Config.java
@@ -14,12 +14,12 @@ public class Config {
 		configuration.save();
 	}
 	
-	static boolean onlyPreventMainThread;
-	static String redirectAddress;
+	static boolean onlyPreventMainThread = false;
+	static String redirectAddress = "127.0.0.1";
 	
 	private static void loadConfiguration() {
-		onlyPreventMainThread = configuration.getBoolean("onlyPreventMainThread", "Thread", false, "Only prevent main thread url access.");
-		redirectAddress = configuration.getString("redirectAddress", "Address", "127.0.0.1", "Redirect address.");
+		onlyPreventMainThread = configuration.getBoolean("onlyPreventMainThread", "Thread", onlyPreventMainThread, "Only prevent main thread url access.");
+		redirectAddress = configuration.getString("redirectAddress", "Address", redirectAddress, "Redirect address.");
 	}
 
 }
